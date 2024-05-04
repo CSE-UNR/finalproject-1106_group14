7ea42a47adc494d61631130dded9aaee3e44af26
1106-Nelson-Faith <faithn@nevada.unr.edu>
2024-05-04 20:51:20 +0000

    changes

--- FinalProject.c
+++ FinalProject.c
@@ -13,7 +13,17 @@ int main(){
 		printf("1. Display an image\n 2. Edit Image\n 3. Exit program\n");
 		scanf("%d", Option);
 	}while(Option != 3);
-	
+	switch(Option){
+		case '1':
+		//Display/load image
+		break;
+		case '2':
+		//Edit image
+		break;
+		default:
+			printf("Goodbye\n");
+		break;
+		}
 	
 	
 
