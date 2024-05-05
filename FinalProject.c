32848a78db871fda81c92d17c8541a5ee1e52274
1106-Nelson-Faith <faithn@nevada.unr.edu>
2024-05-05 19:06:49 +0000

    changes

--- FinalProject.c
+++ FinalProject.c
@@ -6,12 +6,12 @@
 
 
 int main(){
-	int Option;
+	int Option1, Option2;
 
 	do{
 		printf("Welcome to Erinstagram please select an option:\n");
 		printf("1. Display an image\n 2. Edit Image\n 3. Exit program\n");
-		scanf("%d", Option);
+		scanf("%d", Option1);
 	}while(Option != 3);
 	switch(Option){
 		case '1':
@@ -19,6 +19,7 @@ int main(){
 		break;
 		case '2':
 			printf("To crop image enter 4, to brighten image enter 5, and to dim image enter 6\n");
+			scanf("%d", Option2);
 		break;
 		default:
 			printf("Goodbye\n");
